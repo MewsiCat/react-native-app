44f3766850611c35761e1e725b25dbeb1c1a270c
Tofulati <almtho2003@gmail.com>
2023-11-04 23:59:23 -0700

    Added music rec buttons

--- mewsicat/screens/MusicRec.js
+++ mewsicat/screens/MusicRec.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image, Button } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 export default function MusicRec() {
+    const song = "Plaechold";
+    const artist = "mommy"
     
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Music Reccomendation!</Text>
-            <Image source={require('../assets/judebox.gif')} style={styles.img} />
-            <Pressable style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>Return Home</Text>
-            </Pressable>
+            <Image source={require('../assets/wife.jpg')} style={styles.img} />
+            <Text style={styles.song}>{song}</Text>
+            <Text style={styles.artist}>{artist}</Text>
+            <Slider
+                style={{width: '90%', height: '90%', alignSelf:'center', paddingTop:0}}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#d0a060"
+                maximumTrackTintColor="#783621"
+                thumbTintColor='#783621'
+            />
+            <View style={{flexDirection:'row', justifyContent:'center'}}>
+                <Button title='⏪' color='#783621' style={styles.button} />
+                <Button title="▶" color='#783621' style={styles.button} />
+                <Button title='⏩' color='#783621' style={styles.button} />
+            </View>
         </View>
     );
 }
@@ -32,7 +47,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     title: {
-        fontSize: 40,
+        fontSize: 20,
         fontWeight: 'bold',
         color: '#783621',
         alignSelf:'center'
@@ -57,8 +72,23 @@ const styles = StyleSheet.create({
     },
     img: {
         alignSelf:'center',
-        width: 300,
+        width: 200,
         height: 250,
-        margin: 150
+        margin: 50,
+        marginBottom:5
+    },
+    song: {
+        alignSelf:'center',
+        paddingLeft: 20,
+        paddingBottom: 0,
+        fontSize: 30,
+        color:'#783621'
+    },
+    artist: {
+        alignSelf:'center',
+        paddingLeft: 20,
+        paddingBottom: 50,
+        fontSize: 15,
+        color:'#d0a060'
     }
 })
