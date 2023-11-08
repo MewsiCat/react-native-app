bbbcc7bb03b9eb2a7bb2dd071c8816f397609435
Tofulati <almtho2003@gmail.com>
2023-11-07 23:13:00 -0800

    Customized settings page when logged in

--- mewsicat/screens/Settings.js
+++ mewsicat/screens/Settings.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
 import Slider from '@react-native-community/slider';
 import {
     withAuthenticator,
@@ -51,6 +51,34 @@ const SignOutButton = () => {
     }
   };
 
+  var userName = "";
+  var userPic = "";
+
+  async function getUser(){
+    try{
+    const currentUserInfo = await Auth.currentUserInfo();
+    const access_token = currentUserInfo.attributes['custom:spotify_token'];
+    
+    var result = await fetch(
+        `https://api.spotify.com/v1/me`,
+        {
+            method: "GET",
+            headers: { Authorization: "Bearer " + access_token },
+        },
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.display_name)
+            console.log(data.images[0].url)
+            userName = data.display_name;
+            userPic = data.images[0].url;
+        });
+    } catch(err) {
+        console.log(err);
+      } 
+}
+
 export default function Settings() {
     const spotifyController = new SpotifyAPIController();
     //const dispatch = useDispatch();
@@ -90,11 +118,13 @@ export default function Settings() {
         }
       }, [response]);
 
+      getUser();
+
       var isLoggedIn = false;
       var logText = "";
     
       if (checkSpotifyConnected) {
-        logText = "Logged into Spotify"
+        logText = "Logged in as: " + userName;
         isLoggedIn = true;
       } else {
         logText = "Log into Spotify"
@@ -129,8 +159,9 @@ export default function Settings() {
             />
 
             <View style={styles.toBottom}>
-                <Pressable style={styles.buttonContainer} onPress={() => {promptAsync(); playSound();}}>
-                    <Text style={styles.buttonText}>{logText}</Text>
+                <Pressable style={styles.welcome} onPress={() => {promptAsync(); playSound();}}>
+                  <Text style={styles.buttonText}>{logText}</Text>
+                  <Image source={{uri: userPic}} style={styles.img}/>
                 </Pressable>
 
                 <SignOutButton style={styles.signout} onPress={() => {playSound();}}/>
@@ -151,6 +182,18 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         width: '90%'
       },
+    welcome: {
+      alignSelf: 'center',
+      flexDirection: 'row',
+      backgroundColor: '#f0d396',
+      borderColor:'#d0a060',
+      paddingHorizontal: 8,
+      borderWidth: 2,
+      borderRadius: 10,
+      width: '90%',
+      // padding: 5,
+      justifyContent: 'space-evenly'
+    },
     container: {
         backgroundColor: '#f0d396',
         padding: 20,
@@ -177,12 +220,17 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#783621',
         padding: 5,
-        fontSize: 20,
+        fontSize: 15,
         fontWeight: 'bold',
         alignSelf:'center'
     },
     toBottom: {
         gap: 20,
         marginTop: 'auto'
-    }
+    },
+    img: {
+      width: 40,
+      height: 40,
+      borderRadius: 30,
+  },
 })
