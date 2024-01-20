c61d9be7e2abf68780d58ac02535f1f50e2ea94d
Patrick3125 <3125patrick@gmail.com>
2024-01-20 14:28:32 -0800

    shop in progress

--- mewsicat/Components/ShopItem.jsx
+++ mewsicat/Components/ShopItem.jsx
@@ -3,6 +3,7 @@ import { View, Text, Image, StyleSheet, Pressable, Dimensions } from 'react-nati
 import Loading from '../screens/Loading';
 import { Audio } from 'expo-av';
 
+
 async function playSound() {
   const { sound } = await Audio.Sound.createAsync(
     require('../assets/mewsound.mp3')
@@ -12,62 +13,36 @@ async function playSound() {
 }
 
 
-export default function ShopItem({ itemName, itemDescription, itemImg, purchased }) {
+export default function ShopItem({ itemName, itemDescription, itemImg, purchased, equipped, onEquip, price, onPurchase}) {
     return (
-        <View style={[styles.container, !purchased && styles.notPurchasedContainer]}>
+        <View style={[styles.container, equipped && styles.notPurchasedContainer]}>
             <Image source={itemImg} style={styles.IMG} />
             <View style={styles.textContainer}>
-
-{/* //export default function ShopItem({ itemName, itemDescription, imgURI, purchased }) {
-
-//    return (!purchased) ? (
-//        <View style={styles.container}>
-//            <View>
-//  <Image source={{ uri: imgURI }} style={styles.IMG} />
-//            </View>
-//            <View style={{padding: 5}}> */}
-
                 <Text style={styles.name}>
                     {itemName}
                 </Text>
-                {/* <Text style={styles.desc}>
-                    {itemDescription}
-                </Text> */}
             </View>
-            {purchased && (
-                <Pressable style={styles.button} onPress={async() => {playSound()}}>
-                    <Text adjustsFontSizeToFit={true} style={styles.pur}>
-                        Equip
-                    </Text>   
-                </Pressable>
-            )}
+        
+        {purchased ? (
+        <Pressable style={styles.button} onPress={() => { playSound(); onEquip(); }}>
+            <Text adjustsFontSizeToFit={true} style={styles.pur}>
+            {equipped ? 'Unequip' : 'Equip'}
+            </Text>   
+        </Pressable>
+        ) :
+        <Pressable style={styles.button} onPress={() => { playSound(); onPurchase(); }}>
+            <View style={styles.priceTag}>
+            <Image source={require('../assets/tiff/fish.png')} style={styles.currImage}/>
+            <Text adjustsFontSizeToFit={true} style={styles.pur}>
+            {(': ' + price)}
+            </Text> 
+            </View>  
+        </Pressable>
+        }
         </View>
+        
 
     )
-
-//    ) : (
-//        <View style={styles.container}>
-//            <View>
-//                <Image source={{ uri: imgURI }} style={styles.IMG} />
-//            </View>
-//            <View style={{padding: 5}}>
-//                <Text style={styles.name}>
-//                    {itemName}
-//                </Text>
-//               <Text style={styles.desc}>
-//                    {itemDescription}
-//                </Text>
-//            </View>
-//            <View style={{marginLeft: 'auto', alignSelf: 'center'}}>
-//                <Pressable style={styles.buttonD} disabled={true}>
-//                    <Text style={styles.pur}>
-//                        Purchased
-//                    </Text>
-//                </Pressable>
-//            </View>
- //       </View>
-//    );
-
 }
 
 const styles = StyleSheet.create({
@@ -82,12 +57,12 @@ const styles = StyleSheet.create({
         height:'100%'
     },
     notPurchasedContainer: {
-        opacity: 0.5, // Makes the component look darker
+        opacity: 0.5, 
     },
     textContainer: {
         padding: 5,
         alignItems: 'center',
-        height:"35%"
+        // height:"35%"
         
     },
     button: {
@@ -100,7 +75,6 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     pur: {
-        
         color: '#783621',
         fontWeight: 'bold',    
 
@@ -118,5 +92,13 @@ const styles = StyleSheet.create({
         aspectRatio:1,
         // aspectRatio: 0.1,
         borderRadius: 10,
+    },
+    currImage : {
+        maxHeight: 22,
+        maxWidth: 25,
+        marginRight: 'auto'
+    },
+    priceTag : {
+        flexDirection: 'row'
     }
 });
